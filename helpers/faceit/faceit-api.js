7b23d1088a7cd9ac0ecee448a2d96685a6431b15
anactualfox <foxablessc2@gmail.com>
2022-02-03 11:35:20 -0700

    added doc blocks

--- helpers/faceit/faceit-api.js
+++ helpers/faceit/faceit-api.js
@@ -1,9 +1,11 @@
 const { FACEIT_API_KEY } = require("../../config.json");
 const axios = require("axios");
-/*
-Queries faceit api for a player, returns promise
-when resolved this promise contains player data, and other junk from faceit
-*/
+
+/**
+ *Queries faceit api for a player, returns promise
+ *when resolved this promise contains player data, and other junk from faceit
+ *@param {string} username
+ */
 const searchPlayer = async (username) => {
 	const query = `https://open.faceit.com/data/v4/players?nickname=${username}&game=csgo`;
 
@@ -16,11 +18,12 @@ const searchPlayer = async (username) => {
 	return axios.get(query, options);
 };
 
-/*
-Finds user name for a player,
-queries v1 api for last 2000 matches
-returns promise that contains 2000 matches and their data
-*/
+/**
+ *Finds user name for a player,
+ *queries v1 api for last 2000 matches
+ *returns promise that contains 2000 matches and their data
+ *@param {string} username
+ */
 const getMatchHistory = async (username) => {
 	let res = await searchPlayer(username);
 	const query = `https://api.faceit.com/stats/api/v1/stats/time/users/${res.data.player_id.toString()}/games/csgo?size=2000`;
--- helpers/imgur/imgur-api.js
+++ helpers/imgur/imgur-api.js
@@ -1,9 +1,13 @@
 const axios = require("axios");
 const { IMGUR_ID, IMGUR_SECRET } = require("../../config.json");
 
-/*
-Returns 100 images from subreddit gallery as promise
-*/
+/**
+ *Returns 100 images from subreddit gallery as promise
+ *@param {string} subreddit - the subreddit imgur searches through
+ *@param {string} sorting - the sorting method 
+ *@param {string} time - the time range
+ *@param {string} page - the page number
+ */
 const searchImgurSubreddit = async (subreddit, sorting, time, page) => {
   const query = `https://api.imgur.com/3/gallery/r/${subreddit}/${sorting}/${time}/${page}`;
   const options = {
@@ -16,9 +20,13 @@ const searchImgurSubreddit = async (subreddit, sorting, time, page) => {
   return axios.get(query, options);
 };
 
-/*
-Returns the actual images themselves
-*/
+/**
+ *Returns the actual images themselves
+ *@param {string} subreddit - the subreddit imgur searches through
+ *@param {string} sorting - the sorting method 
+ *@param {string} time - the time range
+ *@param {string} page - the page number
+ */
 const findImages = async (subreddit, sorting, time, page, numOfImages) => {
   res = await searchImgurSubreddit(subreddit, sorting, time, page);
 
