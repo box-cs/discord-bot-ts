255d356398c0615c2d1b61996826eaf4db1a025b
anactualfox <foxablessc2@gmail.com>
2022-02-03 07:38:52 -0700

    handles multiple guilds

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildIds, token } = require('./config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -13,6 +13,9 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+//register commands to multiple guilds if needed
+guildIds.forEach(guildId=>{
+  rest.put(Routes.applicationGuildCommands(clientId,guildId), { body: commands })
   .then(() => console.log('Successfully registered application commands.'))
   .catch(console.error);
+})
