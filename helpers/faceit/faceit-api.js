4cb02109162ad1e53af9d299e1fbb8a48a542b96
anactualfox <foxablessc2@gmail.com>
2022-02-06 13:59:32 -0700

    added all-time csgo stats request

--- helpers/faceit/faceit-api.js
+++ helpers/faceit/faceit-api.js
@@ -2,8 +2,8 @@ const { FACEIT_API_KEY } = require("../../config.json");
 const axios = require("axios");
 
 /**
- *Queries faceit api for a player, returns promise
- *when resolved this promise contains player data, and other junk from faceit
+ *Queries faceit api for a player
+ *returns player data, and other junk from faceit
  *@param {string} username
  */
 const searchPlayer = async (username) => {
@@ -17,15 +17,30 @@ const searchPlayer = async (username) => {
 	};
 	return axios.get(query, options);
 };
-
 /**
- *Finds user name for a player,
+ * Queries faceit and returns player's all-time stats in csgo
+ * @param {string} username
+ */
+const searchPlayerStats = async (username) => {
+	const res = await searchPlayer(username);
+	const query = `https://open.faceit.com/data/v4/players/${res.data.player_id.toString()}/stats/csgo`;
+
+	const options = {
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: "Bearer " + FACEIT_API_KEY,
+		},
+	};
+	return axios.get(query, options);
+};
+/**
+ *Finds playerId for a player from username
  *queries v1 api for last 2000 matches
- *returns promise that contains 2000 matches and their data
+ *returns last 2000 matches and their data
  *@param {string} username
  */
 const getMatchHistory = async (username) => {
-	let res = await searchPlayer(username);
+	const res = await searchPlayer(username);
 	const query = `https://api.faceit.com/stats/api/v1/stats/time/users/${res.data.player_id.toString()}/games/csgo?size=2000`;
 
 	return axios.get(query);
@@ -33,5 +48,6 @@ const getMatchHistory = async (username) => {
 
 module.exports = {
 	searchPlayer,
+	searchPlayerStats,
 	getMatchHistory,
 };
