0ef477ed10353804b7c88834c8ee8488b0b8ed94
anactualfox <foxablessc2@gmail.com>
2022-07-22 17:27:52 -0600

    uses new embed builder, fixes breaking changes

--- commands/elo.js
+++ commands/elo.js
@@ -1,49 +1,59 @@
-const { FACEIT_API_KEY } = require("../config.json");
 const faceit = require("../helpers/faceit/faceit-api");
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName("elo")
-		.setDescription("Fetches FACEIT Elo")
-		.addStringOption((option) =>
-			option
-				.setName("username")
-				.setDescription("Enter a faceit username")
-				.setRequired(true)
-		),
+  data: new SlashCommandBuilder()
+    .setName("elo")
+    .setDescription("Fetches FACEIT Elo")
+    .addStringOption((option) =>
+      option
+        .setName("username")
+        .setDescription("Enter a faceit username")
+        .setRequired(true)
+    ),
 
-	async execute(interaction) {
-		await interaction.deferReply();
-		const username = interaction.options.getString("username");
-		try {
-			let res = await faceit.searchPlayer(username);
+  async execute(interaction) {
+    await interaction.deferReply();
+    const username = interaction.options.getString("username");
+    try {
+      let res = await faceit.searchPlayer(username);
+      const player = {
+        name: res.data.nickname.toString(),
+        avatar: res.data.avatar.toString(),
+        csgo: {
+          level: res.data.games.csgo.skill_level.toString(),
+          elo: res.data.games.csgo.faceit_elo.toString(),
+        },
+      };
 
-			const playerData = {
-				name: res.data.nickname.toString(),
-				avatar: res.data.avatar.toString(),
-				csgo: {
-					level: res.data.games.csgo.skill_level.toString(),
-					elo: res.data.games.csgo.faceit_elo.toString(),
-				},
-			};
+      res = await faceit.searchPlayerStats(username);
 
-			embedMessage = `${playerData.name}\n**Elo**: ${playerData.csgo.elo}`;
+      const kd = res.data.lifetime["Average K/D Ratio"];
+      const recentResults = res.data.lifetime["Recent Results"]
+        .map((result) => (result == 1 ? "W" : "L"))
+        .join(" ");
 
-			const messageEmbed = new MessageEmbed()
-				.setColor("#ff5500")
-				.setAuthor({
-					name: `${playerData.name}`,
-					iconURL: `https://beta.leetify.com/assets/images/rank-icons/faceit${playerData.csgo.level}.png`,
-					url: `https://www.faceit.com/en/players/${playerData.name}`,
-				})
-				.setDescription(embedMessage)
-				.setThumbnail(playerData.avatar);
+      const emojiResponses = ["🌌", "⚡", "💀"];
+      const response = kd > 1.3 ? emojiResponses[0] : kd > 1.2 ? emojiResponses[1] : emojiResponses[2];
 
-			await interaction.editReply({ embeds: [messageEmbed] });
-		} catch (err) {
-			await interaction.editReply("Player not found?");
-		}
-	},
+      const messageEmbed = new EmbedBuilder()
+        .setColor("#ff5500")
+        .setAuthor({
+          name: `${player.name} ${response}`,
+          iconURL: `https://beta.leetify.com/assets/images/rank-icons/faceit${player.csgo.level}.png`,
+          url: `https://www.faceit.com/en/players/${player.name}`,
+        })
+        .addFields(
+          { name: "Elo", value: player.csgo.elo, inline: true },
+          { name: "Recent Results", value: recentResults, inline: true },
+        )
+        .setThumbnail(player.avatar)
+        .setTimestamp();;
+
+      await interaction.editReply({ embeds: [messageEmbed] });
+    } catch (err) {
+      await interaction.editReply("Player not found?");
+    }
+  },
 };
