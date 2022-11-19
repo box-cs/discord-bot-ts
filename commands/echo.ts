a35ededb7ee3d800004b4141124a99659ca1cda5
anactualfox <foxablessc2@gmail.com>
2022-11-18 18:24:53 -0700

    commands use typescript

---
+++ commands/echo.ts
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("echo")
+		.setDescription("Echoes input")
+		.addStringOption((option) =>
+			option.setName("input").setDescription("Enter a string").setRequired(true)
+		),
+
+	async execute(interaction) {
+		await interaction.deferReply();
+		const string = interaction.options.getString("input");
+		await interaction.editReply(string);
+	},
+};
+export { };
---
+++ commands/lmgtfy.ts
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("lmgtfy")
+		.setDescription("Let me google that for you")
+		.addStringOption((option) =>
+			option.setName("query").setDescription("Enter a string").setRequired(true)
+		),
+
+	async execute(interaction) {
+		await interaction.deferReply();
+		const query = interaction.options.getString("query");
+		await interaction.editReply(`http://lmgtfy.com/?q=${query}`);
+	},
+};
+export { };
