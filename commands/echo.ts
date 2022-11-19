913d86984d50ea8400cf81f57bedb60f9c2d67cc
anactualfox <foxablessc2@gmail.com>
2022-11-18 21:10:38 -0700

    minor changes

--- commands/echo.ts
+++ commands/echo.ts
@@ -1,16 +1,16 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName("echo")
-		.setDescription("Echoes input")
-		.addStringOption((option) =>
-			option.setName("input").setDescription("Enter a string").setRequired(true)
-		),
+  data: new SlashCommandBuilder()
+    .setName("echo")
+    .setDescription("Echoes input")
+    .addStringOption((option) =>
+      option.setName("input").setDescription("Enter a string").setRequired(true)
+    ),
 
-	async execute(interaction) {
-		await interaction.deferReply();
-		await interaction.editReply(interaction.options.getString("input"));
-	},
+  async execute(interaction) {
+    await interaction.deferReply();
+    await interaction.editReply(interaction.options.getString("input"));
+  },
 };
 export { };
