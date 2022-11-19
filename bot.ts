2630509aa541493022ddac23de4b01b29885991f
anactualfox <foxablessc2@gmail.com>
2022-11-18 22:20:19 -0700

    minor

--- bot.ts
+++ bot.ts
@@ -1,7 +1,6 @@
-
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, Interaction, InteractionType } = require("discord.js");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 const Events = require("./eventCommands/Events");             // Optional
 const EventFactory = require("./eventCommands/EventFactory"); // Optional             
