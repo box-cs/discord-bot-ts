64c50540750436834a0296525ab4be6b1590344c
anactualfox <foxablessc2@gmail.com>
2023-02-22 01:12:59 -0700

    added steam api call to resolve vanity urls

---
+++ api/steam/steam-api.ts
@@ -0,0 +1,30 @@
+const { STEAM_API_KEY } = require("../../config.json");
+import axios from "axios";
+
+const getResolvedSteamID = async (vanityURL: string) => {
+  const query = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${STEAM_API_KEY}&vanityurl=${vanityURL}`;
+  return axios.get(query);
+};
+
+const resolveSteamID = async (url: string) => {
+  // https://steamcommunity.com/profiles/{id64}/
+  // https://steamcommunity.com/id/{vanityId}/
+  const linkType = url.split("/").at(3);
+  let id = url.split("/").at(4);
+
+  if (linkType === "id") {
+    const res = await getResolvedSteamID(id);
+    const { response } = res?.data;
+    if (response && response?.success == 1) {
+      id = response?.steamid;
+    } else {
+      throw new Error("Vanity URL could not be resolved!");
+    }
+  }
+
+  return id;
+};
+
+module.exports = {
+  resolveSteamID,
+};
