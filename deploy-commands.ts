1e6a901d0461d43bd6a3cac93dc9e1bc1eb88f7e
anactualfox <foxablessc2@gmail.com>
2023-05-17 17:19:25 -0600

    typed

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,14 +1,19 @@
+import {
+  RESTPostAPIChatInputApplicationCommandsJSONBody,
+  SlashCommandBuilder,
+} from "discord.js";
 const fs = require("fs");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const { clientId, guildIds, token } = require("./config.json");
 
-const commands: Array<any> = [];
-const commandFiles: Array<string> = fs.readdirSync('./commands')
-  .filter((file: string) => file.endsWith('.ts'));
+const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
+const commandFiles: string[] = fs
+  .readdirSync("./commands")
+  .filter((file: string) => file.endsWith(".ts"));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
+  const command: { data: SlashCommandBuilder } = require(`./commands/${file}`);
   commands.push(command.data.toJSON());
 }
 
@@ -17,10 +22,12 @@ const rest = new REST({ version: "9" }).setToken(token);
 (async () => {
   guildIds.forEach((guildId: string) => {
     rest
-      .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+      .put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: commands,
+      })
       .then(() => console.log("Successfully registered application commands."))
       .catch(console.error);
   });
 })();
 
-export { };
+export {};
