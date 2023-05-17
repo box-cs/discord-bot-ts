f63ef433ac996cee0bd1f54e72de144d908cd49e
anactualfox <foxablessc2@gmail.com>
2023-05-17 17:19:15 -0600

    typed

--- bot.ts
+++ bot.ts
@@ -1,30 +1,35 @@
-const fs = require('node:fs');
-const path = require('node:path');
+import { Interaction, Message } from "discord.js";
+const fs = require("node:fs");
+const path = require("node:path");
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
-const Events = require("./eventCommands/Events");             // Optional
-const EventFactory = require("./eventCommands/EventFactory"); // Optional             
+const Events = require("./eventCommands/Events"); // Optional
+const EventFactory = require("./eventCommands/EventFactory"); // Optional
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
+    GatewayIntentBits.MessageContent,
   ],
 });
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+const pathToCommands = path.join(__dirname, "commands");
+const commandFiles: string[] = fs
+  .readdirSync(pathToCommands)
+  .filter((file: string) => file.endsWith(".ts"));
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
+  const filePath = path.join(pathToCommands, file);
   const command = require(filePath);
   // Set a new item in the Collection with the key as the command name and the value as the exported module
-  if ('data' in command && 'execute' in command) {
+  if ("data" in command && "execute" in command) {
     client.commands.set(command.data.name, command);
   } else {
-    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
   }
 }
 
@@ -35,7 +40,7 @@ client.once("ready", () => {
 
 // messageCreate events are handled like this so I can split my messageCreate commands
 // into another folder and not share it on github
-client.on("messageCreate", async (msg) => {
+client.on("messageCreate", async (msg: Message) => {
   try {
     if (!msg.author.bot) {
       Events.handleEvent(msg); // Optional
@@ -46,17 +51,23 @@ client.on("messageCreate", async (msg) => {
 });
 
 // handles onInteractionCreate events for commands and button events
-client.on("interactionCreate", async (interaction) => {
+client.on("interactionCreate", async (interaction: Interaction) => {
   if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
+  const client = interaction.client;
+  // @ts-ignore
+  const command = client.commands.get(interaction.commandName);
   if (!command)
-    return console.error(`No command matching ${interaction.commandName} was found.`);
+    return console.error(
+      `No command matching ${interaction.commandName} was found.`
+    );
 
   try {
     await command.execute(interaction);
   } catch (error) {
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
   }
 });
 
