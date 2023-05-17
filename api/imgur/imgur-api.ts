3111e42ccec2fb7c00d3f4bfdba6738e3489199c
anactualfox <foxablessc2@gmail.com>
2023-05-17 17:19:44 -0600

    prettier and typed

--- api/imgur/imgur-api.ts
+++ api/imgur/imgur-api.ts
@@ -1,5 +1,5 @@
-const { IMGUR_ID, IMGUR_SECRET } = require("../../config.json");
-import axios from "axios";
+const { IMGUR_ID } = require("../../config.json");
+import axios, { AxiosRequestConfig } from "axios";
 /**
  * Returns 100 images from subreddit gallery as promise
  * @param {string} subreddit - the subreddit imgur searches through
@@ -7,9 +7,14 @@ import axios from "axios";
  * @param {string} time - the time range
  * @param {number} page - the page number
  */
-const searchImgurSubreddit = async (subreddit: string, sorting: string, time: string, page: number) => {
+const searchImgurSubreddit = async (
+  subreddit: string,
+  sorting: string,
+  time: string,
+  page: number
+) => {
   const query = `https://api.imgur.com/3/gallery/r/${subreddit}/${sorting}/${time}/${page}`;
-  const options = {
+  const options: AxiosRequestConfig<any> = {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Client-ID ${IMGUR_ID}`,
@@ -21,19 +26,25 @@ const searchImgurSubreddit = async (subreddit: string, sorting: string, time: st
 
 /**
  * Returns the actual images themselves
- * @param {string} subreddit - subreddit to search 
+ * @param {string} subreddit - subreddit to search
  * @param {string} sorting - the sorting method
  * @param {string} time - the time range
  * @param {number} page - the page number
  */
-const findImages = async (subreddit: string, sorting: string, time: string, page: number, numOfImages: number) => {
+const findImages = async (
+  subreddit: string,
+  sorting: string,
+  time: string,
+  page: number,
+  numOfImages: number
+) => {
   let res = await searchImgurSubreddit(subreddit, sorting, time, page);
 
-  const images: Array<string> = res.data.data.map((image) => ({
+  const images: string[] = res.data.data.map((image: { link: string }) => ({
     link: image.link,
   }));
 
-  let queriedImages: Array<string> = []; //this will be our functions return
+  let queriedImages: string[] = []; // this will be our functions return
 
   // loop in order to fetch random images from request array of images
   for (let i = 0; i < numOfImages; i++) {
