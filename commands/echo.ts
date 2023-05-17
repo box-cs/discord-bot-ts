162ec1ca9e1cccd4cdde840aa3e55ec1a8bd8629
anactualfox <foxablessc2@gmail.com>
2023-05-17 17:19:58 -0600

    these are types

--- commands/echo.ts
+++ commands/echo.ts
@@ -1,16 +1,21 @@
+import {
+  ChatInputCommandInteraction,
+  SlashCommandStringOption,
+} from "discord.js";
+
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("echo")
     .setDescription("Echoes input")
-    .addStringOption((option) =>
+    .addStringOption((option: SlashCommandStringOption) =>
       option.setName("input").setDescription("Enter a string").setRequired(true)
     ),
 
-  async execute(interaction) {
+  async execute(interaction: ChatInputCommandInteraction) {
     await interaction.deferReply();
     await interaction.editReply(interaction.options.getString("input"));
   },
 };
-export { };
+export {};
