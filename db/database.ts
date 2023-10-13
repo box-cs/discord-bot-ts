2a6b3acb1930d6fb7ec38d47c72cd4874072ff56
anactualfox <foxablessc2@gmail.com>
2023-10-12 19:36:05 -0600

    added db connection

---
+++ db/database.ts
@@ -0,0 +1,19 @@
+import { env } from "../lib/config";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: env.get("DB_USER"),
+  database: env.get("DB_NAME"),
+  password: env.get("DB_PASSWORD"),
+  port: env.get("DB_PORT"),
+  host: env.get("DB_HOST"),
+});
+
+export const db = {
+  query: async (text: string, params: (string | string[])[] = []) => {
+    return await pool.query({
+      text,
+      values: params,
+    });
+  },
+};
---
+++ db/seed.ts
@@ -0,0 +1,28 @@
+import { EventHandler } from "../core/eventHandler";
+import { db } from "./database";
+
+const eventsTableSchema = `
+  CREATE TABLE IF NOT EXISTS events (
+    id SERIAL PRIMARY KEY,
+    creatorId VARCHAR(255) NOT NULL,
+    action VARCHAR(255) NOT NULL,
+    guildId VARCHAR(255) NOT NULL,
+    description TEXT,
+    keywords TEXT[]
+  );
+`;
+
+const getEvents = async () => {
+  const res = await db.query(`SELECT * FROM events`, []);
+  console.log(res.rows);
+  return res.rows;
+};
+
+export const initializeDb = async () => {
+  const tables = [eventsTableSchema];
+  await Promise.all(tables.map(async (table) => db.query(table)));
+  const events = await getEvents();
+  for (const event of events) {
+    EventHandler.addEvent(event);
+  }
+};
